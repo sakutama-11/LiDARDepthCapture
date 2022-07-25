2f2c1afc898897045dbe15b780a65eb7b869c8e1
sakutama-11 <sakutama.swinzy8739@gmail.com>
2022-07-25 18:25:28 +0900

    save depth and image

--- LiDARDepth/CameraController.swift
+++ LiDARDepth/CameraController.swift
@@ -8,6 +8,8 @@ An object that configures and manages the capture pipeline to stream video and L
 import Foundation
 import AVFoundation
 import CoreImage
+import UIKit
+import ARKit
 
 protocol CaptureDataReceiver: AnyObject {
     func onNewData(capturedData: CameraCapturedData)
@@ -199,10 +201,18 @@ extension CameraController: AVCapturePhotoCaptureDelegate {
         // Retrieve the image and depth data.
         guard let pixelBuffer = photo.pixelBuffer,
               let depthData = photo.depthData,
-              let cameraCalibrationData = depthData.cameraCalibrationData else { return }
+              let cameraCalibrationData = depthData.cameraCalibrationData,
+              let imageData = photo.fileDataRepresentation(),
+              let photoUIImage = UIImage(data: imageData) else { return }
         
         // Stop the stream until the user returns to streaming mode.
         stopStream()
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyMMddHHmms"
+        dateFormatter.locale = Locale(identifier: "en_US")
+        dateFormatter.timeZone = TimeZone(identifier:  "Asia/Tokyo")
+        let timestr = dateFormatter.string(from: Date())
         
         // Convert the depth data to the expected format.
         let convertedDepth = depthData.converting(toDepthDataType: kCVPixelFormatType_DepthFloat16)
@@ -213,7 +223,92 @@ extension CameraController: AVCapturePhotoCaptureDelegate {
                                       colorCbCr: pixelBuffer.texture(withFormat: .rg8Unorm, planeIndex: 1, addToCache: textureCache),
                                       cameraIntrinsics: cameraCalibrationData.intrinsicMatrix,
                                       cameraReferenceDimensions: cameraCalibrationData.intrinsicMatrixReferenceDimensions)
-        
+        let convertedDepthMap = depthData.converting(toDepthDataType: kCVPixelFormatType_DepthFloat32).depthDataMap
+        let json = wrapEstimateImageData(depthMap: convertedDepthMap, calibration: cameraCalibrationData)
+        writeJson(json: json, filename: timestr)
+        let image = UIImage(cgImage: photoUIImage.cgImage!, scale: photoUIImage.scale, orientation: .up)
+        writeRGBData(image: image, filename: timestr)
         delegate?.onNewPhotoData(capturedData: data)
+    }
+    
+    func convertDepthData(depthMap: CVPixelBuffer) -> [[Float32]] {
+        let width = CVPixelBufferGetWidth(depthMap)
+        let height = CVPixelBufferGetHeight(depthMap)
+        var convertedDepthMap: [[Float32]] = Array(
+            repeating: Array(repeating: 0, count: width),
+            count: height
+        )
+        CVPixelBufferLockBaseAddress(depthMap, CVPixelBufferLockFlags(rawValue: 2))
+        let floatBuffer = unsafeBitCast(
+            CVPixelBufferGetBaseAddress(depthMap),
+            to: UnsafeMutablePointer<Float32>.self
+        )
+        for row in 0 ..< height {
+            for col in 0 ..< width {
+                convertedDepthMap[row][col] = floatBuffer[width * row + col]
+            }
+        }
+        CVPixelBufferUnlockBaseAddress(depthMap, CVPixelBufferLockFlags(rawValue: 2))
+        return convertedDepthMap
+    }
+    
+    func convertLensDistortionLookupTable(lookupTable: Data) -> [Float] {
+        let tableLength = lookupTable.count / MemoryLayout<Float>.size
+        var floatArray: [Float] = Array(repeating: 0, count: tableLength)
+        _ = floatArray.withUnsafeMutableBytes{lookupTable.copyBytes(to: $0)}
+        return floatArray
+    }
+    
+    func wrapEstimateImageData(
+        depthMap: CVPixelBuffer,
+        calibration: AVCameraCalibrationData
+    ) -> Data {
+        let jsonDict: [String : Any] = [
+            "calibration_data" : [
+                "intrinsic_matrix" : (0 ..< 3).map{ x in
+                    (0 ..< 3).map{ y in calibration.intrinsicMatrix[x][y]}
+                },
+                "pixel_size" : calibration.pixelSize,
+                "intrinsic_matrix_reference_dimensions" : [
+                    calibration.intrinsicMatrixReferenceDimensions.width,
+                    calibration.intrinsicMatrixReferenceDimensions.height
+                ],
+                "lens_distortion_center" : [
+                    calibration.lensDistortionCenter.x,
+                    calibration.lensDistortionCenter.y
+                ],
+                "lens_distortion_lookup_table" : convertLensDistortionLookupTable(
+                    lookupTable: calibration.lensDistortionLookupTable!
+                ),
+                "inverse_lens_distortion_lookup_table" : convertLensDistortionLookupTable(
+                    lookupTable: calibration.inverseLensDistortionLookupTable!
+                )
+            ],
+            "depth_data" : convertDepthData(depthMap: depthMap)
+        ]
+        let jsonStringData = try! JSONSerialization.data(
+            withJSONObject: jsonDict,
+            options: .prettyPrinted
+        )
+        return jsonStringData
+    }
+    
+    func writeJson(json: Data, filename: String) {
+        guard let url = try? FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: true).appendingPathComponent(filename + ".json") else { return }
+        print(url)
+        do {
+            try json.write(to: url)
+        } catch let error {
+            print(error)
+        }
+    }
+    func writeRGBData(image: UIImage, filename: String) {
+        let jpgImageData = image.jpegData(compressionQuality: 1.0)
+        guard let url = try? FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: true).appendingPathComponent(filename + ".jpg") else { return }
+        do {
+            try jpgImageData!.write(to: url)
+        } catch let error{
+            print(error)
+        }
     }
 }
